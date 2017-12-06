51c2f855a520ba18ee522e564a4feb3aa4c55327
Brian Jones <beeceej.code@gmail.com>
2017-12-05 22:14:28 -0600

    add comment and restructure

--- system/input/disable_dwt.sh
+++ system/input/disable_dwt.sh
@@ -1,8 +1,8 @@
 # libinput enables touchpad disable-while-typing by default
-# In the Gnome desktop environment this will disable said "feature" 
+# In the Gnome desktop environment this will disable said "feature"
+# Only needed on some laptops
 
 main() {
-  local INPUT_TOGGLE=$1
   gsettings set org.gnome.desktop.peripherals.touchpad disable-while-typing false
 }
 
