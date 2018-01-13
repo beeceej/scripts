c24a07bab13774aa27c1059528442f73ba986b66
Brian Jones <beeceej.code@gmail.com>
2018-01-13 10:47:02 -0600

    bugfixes /  improvement

--- fedora/util/go/install_go.sh
+++ fedora/util/go/install_go.sh
@@ -15,26 +15,34 @@ cleanup() {
 
 download() {
     local url=$1
-    local version=$2
 
     if [[ -d /usr/local/.goconfig/$version/go ]]; then
         echo "version $version already cached, no need to retrieve it again..."
     else
-        wget $url -P "${TMP_DIR}"
-    if    
+        wget $url -P $TMP_DIR
+    fi
 }
 
 unpack() {
     local version=$1
     local arch=$2
 
+    sudo mkdir -p /usr/local/.goconfig/$version
+
+    sudo tar -C /usr/local/.goconfig/$version/ -xf "${TMP_DIR}/${version}.${arch}.tar.gz"
+}
+
+remove_old_install() {
     if [[ -L /usr/local/go || -d /usr/local/go ]]; then
         echo "Removing Previous Go installation.."
         sudo rm -rf /usr/local/go        
     fi
-    sudo mkdir -p /usr/local/.goconfig/$version
+}
 
-    sudo tar -C /usr/local/.goconfig/$version/ -xvf "${TMP_DIR}/${version}.${arch}.tar.gz"
+symlink_to_version() {
+    local version=$1
+
+    echo "linking /usr/local/.goconfig/$version/go -> /usr/local/go"
     sudo ln -s /usr/local/.goconfig/$version/go /usr/local/go
 }
 
@@ -43,8 +51,18 @@ main() {
     local arch=$2
 
     setup
-    download "${GO_HOST_URL_BASE}${version}.${arch}.tar.gz" $2
-    unpack $version $arch
+
+    if [[ -d /usr/local/.goconfig/$version/go ]]; then
+        echo "version $version already cached, no need to retrieve it again..."
+        remove_old_install
+        symlink_to_version $version
+    else
+        download "${GO_HOST_URL_BASE}${version}.${arch}.tar.gz"
+        unpack $version $arch
+        remove_old_install
+        symlink_to_version $version
+    fi
+
     cleanup
 }
 
